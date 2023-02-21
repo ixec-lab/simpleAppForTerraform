df62529c73ed29b736e24ec914068c0ad4862df4
ixec-lab <islem.meghnine06@gmail.com>
2023-02-21 13:45:13 +0100

    UPDATE: adding auto hostname

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # app.py
 from wsgiref.simple_server import make_server
+from socket import gethostname
 
 def simple_app(environ, start_response):
     """Simplest possible application object"""
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return [b"Hello world!\n"]
+    msg = "Hello world!, from {}\n".format(str(gethostname()))
+    return [bytes(msg,'UTF-8')]
 with make_server('', 80, simple_app) as httpd:
     print("Serving on port 80 ...")
     httpd.serve_forever()
